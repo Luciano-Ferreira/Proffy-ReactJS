f819f6e8f0c98f0cb036928d3a6d7f1689da425e
Luciano Ferreira <luciano2023silva@gmail.com>
2020-08-11 20:50:13 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> {/*injeta o html dentro do elemento html*/}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
